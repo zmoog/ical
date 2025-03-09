392f161028b3b5046369c136d50920a1d3413f1d
Maurizio Branca <maurizio.branca@gmail.com>
2025-03-09 07:52:36 +0100

    Add support for JSON output format

--- cmd/root.go
+++ cmd/root.go
@@ -4,10 +4,14 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/zmoog/ws/feedback"
 )
+
+var outputFormat string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -22,6 +26,20 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		switch outputFormat {
+		case "table":
+			feedback.SetFormat(feedback.Table)
+		case "text":
+			feedback.SetFormat(feedback.Text)
+		case "json":
+			feedback.SetFormat(feedback.JSON)
+		default:
+			feedback.Println(fmt.Sprintf("Invalid output format: %s", outputFormat))
+			feedback.Println("Valid formats: table, text, json; using default: table")
+			feedback.SetFormat(feedback.Table)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -43,4 +61,5 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVarP(&outputFormat, "output", "o", "table", "Output format (table, json, yaml)")
 }
